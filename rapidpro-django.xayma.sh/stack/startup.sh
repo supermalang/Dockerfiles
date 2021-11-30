43c0d4a33ce5a09f0a881b99219003b88ef965cc
Elhadji Malang Diedhiou <supermalang@outlook.com>
2021-11-30 16:54:23 +0000

    [UPDATE] Updated rapidpro and mailroom dockerfiles

--- rapidpro-django.xayma.sh/stack/startup.sh
+++ rapidpro-django.xayma.sh/stack/startup.sh
@@ -26,4 +26,7 @@ if [ "x$MANAGEPY_IMPORT_GEOJSON" = "xon" ]; then
 	/venv/bin/python manage.py import_geojson ./geojson/*.json
 	echo "Imported geojson for relation_ids $OSM_RELATION_IDS"
 fi
+
+service nginx start
+
 $STARTUP_CMD
